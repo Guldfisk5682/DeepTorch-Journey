74333b91ef199bfc353f8d1f65f050f4abf70d72
Guldfisk5682 <1394629524@qq.com>
2025-04-27 15:17:03 +0800

    CV Base

--- CV-exam/FCN.py
+++ CV-exam/FCN.py
@@ -5,5 +5,22 @@ import torchvision.models as models
 class SimpleFCN(nn.Module):
     def __init__(self,num_classes) -> None:
         super().__init__()
-        self.backbone=models.resnet18(pretrained=True).children()[:-2]
+        self.backbone = nn.Sequential(*list(models.resnet18(pretrained=True).children())[:-2])
+        self.conv1x1=nn.Conv2d(512,num_classes,kernel_size=1)
+        self.transpose_conv=nn.ConvTranspose2d(num_classes,num_classes,kernel_size=64,padding=16,stride=32)
+
+    def forward(self,X):
+        X=self.backbone(X)
+        X=self.conv1x1(X)
+        X=self.transpose_conv(X)
+        return X
         
+num_classes = 21  # 假设有21个类别
+model = SimpleFCN(num_classes)
+
+# 输入图像
+input_image = torch.randn(1, 3, 224, 224)  # 假设输入图像尺寸为224x224
+
+# 前向传播
+output = model(input_image)
+print(output.shape)
